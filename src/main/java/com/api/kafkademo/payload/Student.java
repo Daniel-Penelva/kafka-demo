ea8df9c74550d04ed2bae445a61469ff84df6eb5
Daniel Penelva <d4n.andrade@gmail.com>
2024-01-09 20:41:22 -0300

    Configurando kafka para JSON Object (criando Student e KafkaJsonProducer) e serialização e deserialização + alterando application.yml

---
+++ src/main/java/com/api/kafkademo/payload/Student.java
@@ -0,0 +1,14 @@
+package com.api.kafkademo.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+
+}
