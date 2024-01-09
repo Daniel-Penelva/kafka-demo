d06fb238b58a82f5bbcf40d15a5a13eab451d251
Daniel Penelva <d4n.andrade@gmail.com>
2024-01-09 02:42:50 -0300

    Criando kafka Producer (KafkaProducer)

---
+++ src/main/java/com/api/kafkademo/producer/KafkaProducer.java
@@ -0,0 +1,22 @@
+package com.api.kafkademo.producer;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String msg) {
+        log.info(format("Sending message to Daniel Topic :: %s", msg));
+        kafkaTemplate.send("Daniel", msg);
+    }
+
+}
